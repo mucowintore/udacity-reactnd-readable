62fd8173b03d7ef95f1fa8a07df1fc84255b1d44
Mucowintore <muco@mit.edu>
2018-02-02 11:26:03 +0200

    Implement a FormModal Component upon which all modals will based

---
+++ frontend/src/components/FormModal.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Dimmer, Button, Icon, Grid, Header, Divider, Segment } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+
+
+class FormModal extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  handleOpen = () => this.setState({ isOpen: true })
+  handleClose = () => this.setState({ isOpen: false })
+
+  render() {
+    const { isOpen } = this.state
+    return (
+      <div>
+        {/* Render a placeholder button with its onClick property set to this.handleOpen */}
+        {
+          React.cloneElement(
+            this.props.placeholderButton,
+            { onClick: this.handleOpen}
+          )
+        }
+
+        <Dimmer
+          page
+          active={isOpen}
+          inverted={true}
+        >
+          <Grid centered>
+            <Grid.Column width={6}>
+              <Segment raised color={this.props.color}>
+                <Header as='h1' content={this.props.title} />
+                <Divider hidden />
+                {/* Render a form with its handleCloseModal property set to this.handleClose */}
+                {
+                  React.cloneElement(
+                    this.props.form,
+                    { handleCloseModal: this.handleClose }
+                  )
+                }
+              </Segment>
+            </Grid.Column>
+          </Grid>
+        </Dimmer>
+      </div>
+    )
+  }
+}
+
+FormModal.propTypes = {
+  placeholderButton: PropTypes.element,
+  formColor: PropTypes.string,
+  formTitle: PropTypes.string,
+  form: PropTypes.element,
+}
+
+export default FormModal
