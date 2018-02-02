1431ec1fbe361f2f6c2ba6d6d70f74a85832bcf0
Mucowintore <muco@mit.edu>
2018-02-02 11:53:39 +0200

    Implement AddCommentForm

---
+++ frontend/src/components/AddCommentForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Form, Icon, Button } from 'semantic-ui-react'
+
+
+const AddCommentForm = () => (
+  <Form>
+    <Form.Input fluid label='Author' name='author' placeholder='Enter your name' />
+    <Form.TextArea label='Content' name='body' placeholder='Type here...' />
+    <Form.Group>
+          <Form.Button icon color='green'>
+            <Icon name='checkmark' /> Submit
+          </Form.Button>
+          <Button basic compact color='red'>
+            <Icon name='remove'/> Cancel
+          </Button>
+    </Form.Group>
+  </Form>
+)
+
+export default AddCommentForm
