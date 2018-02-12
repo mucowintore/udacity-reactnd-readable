8582712fc60459099ea004173636298636f22087
Mucowintore <muco@mit.edu>
2018-02-12 20:14:19 +0200

    Implement Comment actions and their reducer logic

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -4,6 +4,22 @@ export const FETCH_COMMENTS = 'FETCH_COMMENTS'
 export const FETCH_COMMENTS_SUCCESS = 'FETCH_COMMENTS_SUCCESS'
 export const FETCH_COMMENTS_FAILURE = 'FETCH_COMMENTS_FAILURE'
 
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS'
+export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE'
+
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const EDIT_COMMENT_SUCCESS = 'EDIT_COMMENT_SUCCESS'
+export const EDIT_COMMENT_FAILURE = 'EDIT_COMMENT_FAILURE'
+
+export const VOTE_COMMENT = 'VOTE_COMMENT'
+export const VOTE_COMMENT_FAILURE = 'VOTE_COMMENT_FAILURE'
+export const VOTE_COMMENT_SUCCESS = 'VOTE_COMMENT_SUCCESS'
+
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const DELETE_COMMENT_SUCCESS = 'DELETE_COMMENT_SUCCESS'
+export const DELETE_COMMENT_FAILURE = 'DELETE_COMMENT_FAILURE'
+
 export function fetchComments (parentId) {
   return (dispatch) => {
     ReadableAPI.getComments(parentId).then(
@@ -21,6 +37,90 @@ export function fetchCommentsSuccess (fetchedComments) {
 export function fetchCommentsFailure (error) {
   return {
     type: FETCH_COMMENTS_FAILURE,
+    error,
+  }
+}
+
+export function addComment (commentData) {
+  return (dispatch) => {
+    ReadableAPI.addComment(commentData).then(
+      addedComment => dispatch(addCommentSuccess(addedComment)),
+      error => dispatch(addCommentFailure(error))
+    )
+  }
+}
+export function addCommentSuccess (addedComment) {
+  return {
+    type: ADD_COMMENT_SUCCESS,
+    addedComment,
+  }
+}
+export function addCommentFailure (error) {
+  return {
+    type: ADD_COMMENT_FAILURE,
+    error,
+  }
+}
+
+export function editComment (commentId, comment) {
+  return (dispatch) => {
+    ReadableAPI.editComment(commentId, comment).then(
+      editedComment => dispatch(editCommentSuccess(editedComment)),
+      error => dispatch(editCommentFailure(error))
+    )
+  }
+}
+export function editCommentSuccess (editedComment) {
+  return {
+    type:EDIT_COMMENT_SUCCESS,
+    editedComment,
+  }
+}
+export function editCommentFailure (error) {
+  return {
+    type: EDIT_COMMENT_FAILURE,
+    error,
+  }
+}
+
+export function voteComment (commentId, option) {
+  return (dispatch) => {
+    ReadableAPI.voteComment(commentId, option).then(
+      votedComment => dispatch(editCommentSuccess(votedComment)),
+      error => dispatch(voteCommentFailure(error))
+    )
+  }
+}
+export function voteCommentSuccess (votedComment) {
+  return {
+    type: VOTE_COMMENT_SUCCESS,
+    votedComment,
+  }
+}
+export function voteCommentFailure (error) {
+  return {
+    type: VOTE_COMMENT_FAILURE,
+    error,
+  }
+}
+
+export function deleteComment (commentId) {
+  return (dispatch) => {
+    ReadableAPI.deleteComment(commentId).then(
+      success => dispatch(deleteCommentSuccess(commentId)),
+      error => dispatch(deleteCommentFailure(error))
+    )
+  }
+}
+export function deleteCommentSuccess (deletedCommentId) {
+  return {
+    type: DELETE_COMMENT_SUCCESS,
+    deletedCommentId,
+  }
+}
+export function deleteCommentFailure (error) {
+  return {
+    type: DELETE_COMMENT_FAILURE,
     error,
   }
 }
--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -1,8 +1,17 @@
 import {
-  FETCH_COMMENTS_SUCCESS
+  FETCH_COMMENTS_SUCCESS,
+  ADD_COMMENT_SUCCESS,
+  EDIT_COMMENT_SUCCESS,
+  VOTE_COMMENT_SUCCESS,
+  DELETE_COMMENT_SUCCESS,
 } from '../actions/comments'
 
 import { toIdDictionary } from '../utils'
+
+export const getCommentIdsByParent = (parentId, comments) => {
+  return Object.keys(comments)
+          .filter(id => comments[id].parentId === parentId && !comments[id].deleted)
+}
 
 const comments = (comments = {}, action) => {
   switch(action.type) {
@@ -13,39 +22,40 @@ const comments = (comments = {}, action) => {
         [parentId]: toIdDictionary(fetchedComments)
       }
 
+    case ADD_COMMENT_SUCCESS:
+      const { addedComment } = action
+      return {
+        ...comments,
+        [addedComment.id]: addedComment,
+      }
+
+    case EDIT_COMMENT_SUCCESS:
+      const { editedComment } = action
+      return {
+        ...comments,
+        [editedComment.id]: editedComment,
+      }
+
+    case VOTE_COMMENT_SUCCESS:
+      const { votedComment } = action
+      return {
+        ...comments,
+        [votedComment.id]: votedComment,
+      }
+
+    case DELETE_COMMENT_SUCCESS:
+      const { deletedCommentId } = action
+      return {
+        ...comments,
+        [deletedCommentId]: {
+          ...comments[deletedCommentId],
+          deleted: true,
+        }
+      }
+
     default:
       return comments
   }
 }
-
-// console.log(comments(
-//   {},
-//   {
-//     type: FETCH_COMMENTS_SUCCESS,
-//     fetchedComments: [
-//         {
-//             "id": "894tuq4ut84ut8v4t8wun89g",
-//             "parentId": "8xf0y6ziyjabvozdd253nd",
-//             "timestamp": 1468166872634,
-//             "body": "Hi there! I am a COMMENT.",
-//             "author": "thingtwo",
-//             "voteScore": 6,
-//             "deleted": false,
-//             "parentDeleted": false
-//         },
-//         {
-//             "id": "8tu4bsun805n8un48ve89",
-//             "parentId": "8xf0y6ziyjabvozdd253nd",
-//             "timestamp": 1469479767190,
-//             "body": "Comments. Are. Cool.",
-//             "author": "thingone",
-//             "voteScore": -5,
-//             "deleted": false,
-//             "parentDeleted": false
-//         }
-//     ],
-//     parentId: "8xf0y6ziyjabvozdd253nd",
-//   }
-// ))
 
 export default comments
