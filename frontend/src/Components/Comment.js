14d5094d86af98090c386e8cb1018335b3537e85
Mucowintore <muco@mit.edu>
2018-02-13 19:44:38 +0200

    Implement the Vote Comment feature in the Post Detail View

--- frontend/src/Components/Comment.js
+++ frontend/src/Components/Comment.js
@@ -5,62 +5,54 @@ import { Comment as CommentUI, Icon } from 'semantic-ui-react'
 import FormModal from './FormModal'
 import EditCommentForm from './EditCommentForm'
 import { timeElapsed } from '../utils'
+import { voteComment } from '../actions/comments'
 
 
-// const sampleComments = {
-//   "894tuq4ut84ut8v4t8wun89g": {
-//     id: '894tuq4ut84ut8v4t8wun89g',
-//     parentId: "8xf0y6ziyjabvozdd253nd",
-//     timestamp: 1468166872634,
-//     body: 'Hi there! I am a COMMENT.',
-//     author: 'thingtwo',
-//     voteScore: 6,
-//     deleted: false,
-//     parentDeleted: false
-//   },
-//   "8tu4bsun805n8un48ve89": {
-//     id: '8tu4bsun805n8un48ve89',
-//     parentId: "8xf0y6ziyjabvozdd253nd",
-//     timestamp: 1469479767190,
-//     body: 'Comments. Are. Cool.',
-//     author: 'thingone',
-//     voteScore: -5,
-//     deleted: false,
-//     parentDeleted: false
-//   }
-// }
 
-const Comment = (props) => {
-  const {
-    author,
-    timestamp,
-    body,
-    voteScore
-  } = props
+class Comment extends React.Component {
+  handleUpVote = () => {
+    this.props.voteComment(this.props.id, 'upVote')
+  }
 
-  return (
-    <CommentUI>
-      <CommentUI.Content>
-        <CommentUI.Author as='a' size='big'>{author}</CommentUI.Author>
-        <CommentUI.Metadata>
-          <div>{timeElapsed(timestamp)}</div>
-          <div>
-            <Icon link name='angle down'/><Icon name='star' color='yellow' />{voteScore} <Icon link name='angle up' />
-          </div>
-          <FormModal
-            title='Edit Comment'
-            color='green'
-            form={<EditCommentForm/>}
-            placeholder={
-              <Icon link name='pencil' color='green' />
-            }
-          />
-          <Icon link name='trash outline' color='red' />
-        </CommentUI.Metadata>
-        <CommentUI.Text>{body}</CommentUI.Text>
-      </CommentUI.Content>
-    </CommentUI>
-  )
+  handleDownVote = () => {
+    this.props.voteComment(this.props.id, 'downVote')
+  }
+
+  render() {
+    const {
+      author,
+      timestamp,
+      body,
+      voteScore
+    } = this.props
+
+    return (
+      <CommentUI>
+        <CommentUI.Content>
+          <CommentUI.Author as='a' size='big'>{author}</CommentUI.Author>
+          <CommentUI.Metadata>
+            <div>{timeElapsed(timestamp)}</div>
+            <div>
+              <Icon link name='angle down' onClick={this.handleDownVote}/>
+              <Icon name='star' color='yellow' />
+              {voteScore}{' '}
+              <Icon link name='angle up' onClick={this.handleUpVote}/>
+            </div>
+            <FormModal
+              title='Edit Comment'
+              color='green'
+              form={<EditCommentForm/>}
+              placeholder={
+                <Icon link name='pencil' color='green' />
+              }
+            />
+            <Icon link name='trash outline' color='red' />
+          </CommentUI.Metadata>
+          <CommentUI.Text>{body}</CommentUI.Text>
+        </CommentUI.Content>
+      </CommentUI>
+    )
+  }
 }
 
 function mapStateToProps({ comments }, { id }) {
@@ -68,4 +60,4 @@ function mapStateToProps({ comments }, { id }) {
 }
 
 
-export default connect(mapStateToProps)(Comment)
+export default connect(mapStateToProps, { voteComment })(Comment)
